306b86f06de46da7df344ad7e84b6c76ee162331
rajkumar bawane <rajkumarbawane10@gmail.com>
2024-02-21 13:03:43 +0530

    loop

---
+++ script.py
@@ -0,0 +1,50 @@
+x = 10
+print(x)
+
+x = 100
+print(x)
+
+#program 2
+s = 9
+t = 3
+
+print(s + t)
+print(s - t)
+print(s * t)
+print(s / t)
+print(s % t)
+
+q = 12
+r = 4
+
+print(s + t)
+print(s - t)
+print(s * t)
+print(s / t)
+print(s % t)
+
+def calculator(x,y):
+   print(x+y) 
+   print(x-y) 
+   print(x*y) 
+   print(x/y) 
+   print(x%y) 
+calculator(20,10)
+calculator(5,3)
+
+#function without parameter and without return type
+
+def addA():
+   print(9+9)
+addA()
+addA()
+addA()
+addA()
+
+def addB(x,y):
+   print(x+y)
+addB(12,2)
+addB(10,12)
+addB(20,10)
+
+#
